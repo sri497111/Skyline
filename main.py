f104b72c8ad70963a4d715559806055cecfb4dd3
sri497111 <sricharan.athanti@gmail.com>
2026-06-04 12:55:10 -0500

    Added blurred elements and started on ui

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from PyQt5.QtWidgets import (
     QApplication, QMainWindow, QLabel, QWidget, QPushButton, 
 )
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QFontDatabase
+from PyQt5.QtGui import QFontDatabase, QPixmap
 from PyQt5 import QtWidgets
+from PyQt5.QtSvg import QSvgWidget
 
 # Modules
 from location import *
 from retrieve import Weather
-from ui_engine import text, button, poppins
+from ui_engine import Card, text, button, poppins
 
 # System
 import sys
@@ -31,21 +32,36 @@ SIZE = (975, 610)
 class MainWindow(QMainWindow):
     def __init__(self, scale):
         super().__init__()
-        
         self.scale = scale
-        
+        self.setFixedSize(self.s(SIZE[0]), self.s(SIZE[1]))
+        self.windowsize = (self.s(SIZE[0]), self.s(SIZE[1]))
         self.setStyleSheet("""
             QMainWindow {
-                border-image: url('lightpartly.png') 0 0 0 0 stretch stretch;
+                border-image: url('./Backgrounds/cloudy/clouds.png') 0 0 0 0 stretch stretch;
             }
         """)
         
-        self.weather = Weather("Phoenix")
-        self.weather.check()
-        test = text(self.weather.retrieve()['weather'][0]['main'], "white", poppins("semi bold"), self.s(50), self)
-        test.move(self.s(100), self.s(100))
         
-        self.setFixedSize(self.s(SIZE[0]), self.s(SIZE[1]))
+        
+        self.element = QPixmap("./Backgrounds/cloudy/elementblur.png")
+        
+        self.mainbox = Card(self, self.element, self.s(self.windowsize[0]/2)-450, self.s(250), self.s(900), self.s(400))
+        
+        self.weather = Weather(current_location())
+        print(current_location())
+        self.weather.check()
+        weather = self.weather.retrieve()
+        print(weather)
+        test = text(weather['weather'][0]['main'], "white", poppins("semi bold"), self.s(50), self)
+        test.move(210, 100)
+        
+        self.condition_icon = QSvgWidget(self)
+        self.condition_icon.load("./Icons/cloudy.svg")
+        self.condition_icon.setGeometry(self.s(90), self.s(90), self.s(120), self.s(120))
+        
+        temperature = text(str(weather['main']['temp']), "white", poppins("semi bold"), self.s(50), self)
+        temperature.move(510, 100)
+        
         print(self.s(SIZE[0]), self.s(SIZE[1]))
         
         
--- ui_engine.py
+++ ui_engine.py
@@ -1,6 +1,39 @@
-from PyQt5.QtWidgets import QLabel
-from PyQt5.QtGui import QFont, QFontDatabase
+from PyQt5.QtWidgets import QLabel, QFrame
+from PyQt5.QtGui import QFont, QFontDatabase, QPixmap, QRegion, QPainterPath
 from PyQt5 import QtWidgets
+from PyQt5.QtCore import Qt
+
+
+
+class Card(QFrame):
+    def __init__(self, parent, pixmap, x, y, w, h):
+        super().__init__(parent)
+        self.setGeometry(x, y, w, h)
+        
+        path = QPainterPath()
+        path.addRoundedRect(0, 0, w, h, 55, 55)
+        self.setMask(QRegion(path.toFillPolygon().toPolygon()))
+        
+        scaled = pixmap.scaled(parent.width(), parent.height(), Qt.IgnoreAspectRatio, Qt.SmoothTransformation)
+        self.crop = scaled.copy(x, y, w, h)
+        
+        self.bg = QLabel(self)
+        self.bg.setGeometry(0, 0, w, h)
+        self.bg.setScaledContents(True)
+        self.bg.setPixmap(self.crop)
+        
+        
+        
+        self.bg.show()
+        
+        self.dark = QLabel(self)
+        self.dark.setGeometry(0, 0, w, h)
+        self.dark.setStyleSheet("""
+                background: rgba(0,0,0,30);
+                border-radius: 55px;
+        """)
+        self.dark.raise_()
+        self.show()
 
 def poppins(weight):
     weight = str(weight).title().replace(" ", "")
